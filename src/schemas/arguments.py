45d9dcc640e34087d5df2f433fcd517b2392a3f0
Armando <ikkimasi@gmail.com>
2023-07-16 23:17:03 -0400

    fix model_name conflict

--- src/schemas/arguments.py
+++ src/schemas/arguments.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 
 class Arguments(BaseModel):
-    model_name: Optional[str]
+    model: Optional[str]
     outscale: Optional[int]
     tile: Optional[str]
     face_enhance: Optional[bool]
