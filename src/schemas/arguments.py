b02d9910f4c377686baeb3837648767e11335e74
Armando <ikkimasi@gmail.com>
2023-07-17 02:20:42 -0400

    change schema to pydantic v2

--- src/schemas/arguments.py
+++ src/schemas/arguments.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 class Arguments(BaseModel):
-    model: Optional[str]
-    outscale: Optional[int]
-    tile: Optional[str]
-    face_enhance: Optional[bool]
-    ext: Optional[str]
-    dn: Optional[float]
+    model: str = Field(default='RealESRGAN_x4plus')
+    outscale: int = Field(default=2)
+    tile: int = Field(default=0)
+    face_enhace: bool = Field(default=False)
+    ext: str = Field(default="auto")
+    dn: float = Field(default=1)
