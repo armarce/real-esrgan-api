79f06011a5a7bd859a26265ab6b0f32a1f448446
Armando <ikkimasi@gmail.com>
2023-07-17 08:32:31 -0400

    add uvicorn

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routes.real_esrgan_route import real_esrgan
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 app = FastAPI(
     title="Real ESRGAN API",
@@ -15,3 +16,6 @@ app = FastAPI(
 #app.mount("/results", StaticFiles(directory="/home/real-esrgan-api/results"), name="results")
 
 app.include_router(real_esrgan)
+print(__name__)
+if __name__ == "__main__":
+   uvicorn.run("app:app", host="0.0.0.0", port=3000, reload=True)
